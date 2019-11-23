357b777647b393705b967213dd9727e821b47d64
Kim Juhui <kjh107704@gmail.com>
2019-11-23 14:52:33 +0900

    fix ShowQuiz

--- code/ShowQuiz.js
+++ code/ShowQuiz.js
@@ -2,6 +2,22 @@ module.exports.function = function showQuiz (quizlist, answer, ordinary) {
 
   let quiz = null;
 
+  if(answer != undefined)
+  {
+    if(String(quiz.correct_answer) == String(answer)) {
+      quiz.result = true;
+      quizlist.correctNum += 1;
+    }
+
+    for(let i = 0; i < quiz.answers_list.length; i++){
+      if(String(quiz.answers_list[i]) == String(answer)){
+        quiz.selected = i;
+        break;
+      }   
+    }
+    quizlist.nowNum += 1;
+  }
+
   if(ordinary != undefined && ordinary < 5 && ordinary > 0) {
     quiz = quizlist.quizzes[quizlist.nowNum];
 

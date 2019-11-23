06605ad6de190c9f536371ce29fecc72a8db1f2c
Kim Juhui <kjh107704@gmail.com>
2019-11-23 13:55:53 +0900

    add ShowQuiz, views

--- code/ShowQuiz.js
+++ code/ShowQuiz.js
@@ -1,3 +1,17 @@
 module.exports.function = function showQuiz (quizlist, answer, ordinary) {
-  return {}
+
+  let quiz = null;
+
+  if(ordinary != undefined && ordinary < 5 && ordinary > 0) {
+    quiz = quizlist.quizzes[quizlist.nowNum];
+
+    if(String(quiz.correct_answer) == String(quiz.answer_list[ordinary-1])) {
+      quiz.result = true;
+      quizlist.correctNum += 1;
+    }
+
+    quiz.selected = ordinary - 1;
+    quizlist.nowNum += 1;
+  }
+  return quizlist;
 }

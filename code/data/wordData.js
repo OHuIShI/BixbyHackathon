dc74161f11ad375c471c88ca901febcdeae30ab0
siraegi <shihabbong11@gmail.com>
2019-11-23 12:17:25 +0900

    data

---
+++ code/data/wordData.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // date의 경우 각자 날짜에 맞게 변경하여 사용하시기 바랍니다.
+  Words : [
+    {
+      word : "arbitraty",
+      mean : "임의적인"
+    },
+    {
+      word : "unarmed",
+      mean : "무기를 가지고 있지 않은"
+    },
+    {
+      word : "burn out",
+      mean : "불에 타다"
+    }
+  ]
+}
